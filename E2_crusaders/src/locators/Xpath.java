46edfb8279606d2cadb98237961343911eca08e5
PiyushAhir21 <vasudevahir21@gmail.com>
2025-01-08 20:37:06 +0530

    CssSelector and Xpath

---
+++ E2_crusaders/src/locators/Xpath.java
@@ -0,0 +1,21 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Xpath {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://demowebshop.tricentis.com/");
+		
+//		driver.findElement(By.xpath("//input[@id='small-searchterms]")).sendKeys("hey");
+
+		driver.findElement(By.xpath("//input[contains(text(),'Log ')]")).sendKeys("hey");
+
+	}
+
+}
