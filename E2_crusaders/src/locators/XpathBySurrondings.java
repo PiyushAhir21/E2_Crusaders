86e88513cba42c37dbabc87edc3378eb097957e3
PiyushAhir21 <vasudevahir21@gmail.com>
2025-01-09 19:54:16 +0530

    xpath by surroundings

---
+++ E2_crusaders/src/locators/XpathBySurrondings.java
@@ -0,0 +1,19 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathBySurrondings {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("C:\\Users\\User\\git\\E2_crusaders\\E2_crusaders\\src\\html\\Surroundings.html");
+		
+		driver.findElement(By.xpath("//label[text()='Male']/../../span/input"));
+//		driver.findElement(By.xpath("//label[text()='Male']/../..//input"));	;
+	}
+
+}
